dcf02f23f39970e7bb7eac8ee4f9127a934c4fd0
sean-adler <sean-adler@users.noreply.github.com>
2016-03-27 23:31:18 -0700

    Update __init__.py

---
+++ django_graphql/__init__.py
@@ -0,0 +1,17 @@
+# flake8: noqa
+from .lib import (
+    DjangoSchema,
+    DjangoType,
+    mutation,
+    prefetch,
+    TypeRegistry
+)
+
+__version__ = '0.0.1'
+__all__ = [
+    'DjangoSchema',
+    'DjangoType',
+    'mutation',
+    'prefetch',
+    'TypeRegistry'
+]
