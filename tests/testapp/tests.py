e7008f4cdf96402632f7e1f093236408c6436224
Sean Adler <sean@counsyl.com>
2016-08-20 13:06:11 -0700

    Add 'DjangoDebugPlugin' (sql debugger) test

--- tests/testapp/tests.py
+++ tests/testapp/tests.py
@@ -1,6 +1,9 @@
 from django.test import TestCase
 from django.utils import timezone
 
+from django_graphql.lib import DjangoSchema
+from django_graphql.sql_debug import DjangoDebugPlugin
+
 from models import Container
 from models import Item
 from models import ItemMovement
@@ -196,3 +199,123 @@ class GraphQLExecutionTests(TestCase):
 
     def test_prefetch(self):
         pass
+
+    def test_debug_sql(self):
+        """
+        Tests that `DjangoDebugPlugin` properly instruments SQL queries
+        executed by GraphQL schema.
+        """
+        # Add DjangoDebug plugin without mutating original schema.
+        sql_debug_schema = DjangoSchema(schema.registry, [DjangoDebugPlugin()])
+        result = sql_debug_schema.execute("""
+            {
+              item(name: "item_4") {
+                id,
+                name,
+                containers {
+                  id,
+                  name,
+                  items {
+                    id,
+                    name
+                  }
+                }
+              },
+              __debug {
+                query_count,
+                queries {
+                  sql,
+                }
+              }
+            }
+        """)
+
+        self.assertDictEqual(
+            result.data,
+            {
+                '__debug': {
+                    'query_count': 4,
+                    'queries': [
+                        {
+                            'sql': (
+                                'QUERY = u\'SELECT (1) AS "a" FROM "testapp_item" '
+                                'WHERE "testapp_item"."name" = %s '
+                                'LIMIT 1\' - PARAMS = (u"\'item_4\'",)'
+                            )
+                        },
+                        {
+                            'sql': (
+                                'QUERY = u\'SELECT "testapp_item"."id", '
+                                '"testapp_item"."name" FROM "testapp_item" '
+                                'WHERE "testapp_item"."name" = %s '
+                                'LIMIT 1\' - PARAMS = (u"\'item_4\'",)'
+                            )
+                        },
+                        {
+                            'sql': (
+                                'QUERY = u\'SELECT ("testapp_itemmovement"."item_id") '
+                                'AS "_prefetch_related_val_item_id", "testapp_container"."id", '
+                                '"testapp_container"."name" '
+                                'FROM "testapp_container" '
+                                'INNER JOIN "testapp_itemmovement" '
+                                'ON ( "testapp_container"."id" = "testapp_itemmovement"."container_id" ) '
+                                'WHERE "testapp_itemmovement"."item_id" IN (%s)\' - PARAMS = (u\'5\',)'
+                            )
+                        },
+                        {
+
+                            'sql': (
+                                'QUERY = u\'SELECT ("testapp_itemmovement"."container_id") '
+                                'AS "_prefetch_related_val_container_id", "testapp_item"."id", '
+                                '"testapp_item"."name" '
+                                'FROM "testapp_item" '
+                                'INNER JOIN "testapp_itemmovement" '
+                                'ON ( "testapp_item"."id" = "testapp_itemmovement"."item_id" ) '
+                                'WHERE "testapp_itemmovement"."container_id" IN (%s, %s)\' - PARAMS = (u\'1\', u\'2\')'
+                            )
+                        }
+                    ]
+                },
+            'item': {
+                 'containers': [
+                    {
+                        'id': 1,
+                        'name': 'container_0',
+                        'items': [
+                            {
+                                'id': 1,
+                                'name': 'item_0'
+                            },
+                            {
+                                'id': 2,
+                                'name': 'item_1'
+                            },
+                            {
+                                'id': 3,
+                                'name': 'item_2'
+                            },
+                            {
+                                'id': 4,
+                                'name': 'item_3'
+                            },
+                            {
+                                'id': 5,
+                                'name': 'item_4'
+                            }
+                        ],
+                    },
+                    {
+                        'id': 2,
+                        'name': 'container_1',
+                        'items': [
+                            {
+                                'id': 5,
+                                'name': 'item_4'
+                            }
+                        ]
+                    }
+                ],
+                'id': 5,
+                'name': 'item_4'
+            }
+        })
